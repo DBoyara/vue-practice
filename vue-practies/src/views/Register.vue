826e5469cf2db1c317fee6e72374deb620961652
Бояршин Денис Игоревич <d.boyarshin@2Gis.ru>
2021-06-10 20:42:16 +0500

    add register api

--- vue-practies/src/views/Register.vue
+++ vue-practies/src/views/Register.vue
@@ -4,21 +4,40 @@
             <div class="row">
                 <div class="col-md-6 offset-md-3 col-xs-12">
                     <h1 class="text-xs-center">Sign up</h1>
-<!--                    <p class="text-xs-center">-->
-<!--                        <router-link :to="{ name: 'login' }"> Need an account? </router-link>-->
-<!--                    </p>-->
+                    <p class="text-xs-center">
+<!--                        <router-link :to="{name: 'login'}">-->
+<!--                            Need an account?-->
+<!--                        </router-link>-->
+                    </p>
                     VALIDATION ERRORS
                     <form @submit.prevent="onSubmit">
                         <fieldset class="form-group">
-                            <input class="form-control form-control-lg" type="text" placeholder="Username" />
+                            <input
+                                class="form-control form-control-lg"
+                                type="text"
+                                placeholder="Username"
+                            />
                         </fieldset>
                         <fieldset class="form-group">
-                            <input class="form-control form-control-lg" type="text" placeholder="Email" />
+                            <input
+                                class="form-control form-control-lg"
+                                type="text"
+                                placeholder="Email"
+                            />
                         </fieldset>
                         <fieldset class="form-group">
-                            <input class="form-control form-control-lg" type="password" placeholder="Password" />
+                            <input
+                                class="form-control form-control-lg"
+                                type="password"
+                                placeholder="Password"
+                            />
                         </fieldset>
-                        <button class="btn btn-lg btn-primary pull-xs-right" :disabled="isSubmitting">Sign Up</button>
+                        <button
+                            class="btn btn-lg btn-primary pull-xs-right"
+                            :disabled="isSubmitting"
+                        >
+                            Sign Up
+                        </button>
                     </form>
                 </div>
             </div>
@@ -31,13 +50,21 @@ export default {
     name: 'McvRegister',
     computed: {
         isSubmitting() {
-            return this.$store.state.auth.isSubmitting;
-        },
+            return this.$store.state.auth.isSubmitting
+        }
     },
     methods: {
         onSubmit() {
-            this.$store.commit('registerStart');
-        },
-    },
-};
+            this.$store
+                .dispatch('register', {
+                    email: 'gweg33wgwd@gwegwe.com',
+                    username: 'gegs33dgsd',
+                    password: 'gdsgsdggrege'
+                })
+                .then(result => {
+                    console.log('result from register action', result)
+                })
+        }
+    }
+}
 </script>
