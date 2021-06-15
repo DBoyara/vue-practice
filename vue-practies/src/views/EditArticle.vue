8b42b5e1b6b3f153c4e7022c20c64d985e3a40bf
DBoyara <boyarshin.den@yandex.ru>
2021-06-15 13:44:21 +0500

    re format

--- vue-practies/src/views/EditArticle.vue
+++ vue-practies/src/views/EditArticle.vue
@@ -13,51 +13,49 @@
 </template>
 
 <script>
-import {mapState} from 'vuex'
+import { mapState } from 'vuex';
 
-import McvArticleForm from '../components/ArticleForm'
-import McvLoading from '../components/Loading'
-import {actionTypes} from '../store/modules/editArticle'
+import McvArticleForm from '../components/ArticleForm';
+import McvLoading from '../components/Loading';
+import { actionTypes } from '../store/modules/editArticle';
 
 export default {
     name: 'McvEditArticle',
     components: {
         McvArticleForm,
-        McvLoading
+        McvLoading,
     },
     computed: {
         ...mapState({
-            isLoading: state => state.editArticle.isLoading,
-            article: state => state.editArticle.article,
-            isSubmitting: state => state.editArticle.isSubmitting,
-            validationErrors: state => state.editArticle.validationErrors
+            isLoading: (state) => state.editArticle.isLoading,
+            article: (state) => state.editArticle.article,
+            isSubmitting: (state) => state.editArticle.isSubmitting,
+            validationErrors: (state) => state.editArticle.validationErrors,
         }),
         initialValues() {
             if (!this.article) {
-                return null
+                return null;
             }
             return {
                 title: this.article.title,
                 description: this.article.description,
                 body: this.article.body,
-                tagList: this.article.tagList
-            }
-        }
+                tagList: this.article.tagList,
+            };
+        },
     },
     mounted() {
         this.$store.dispatch(actionTypes.getArticle, {
-            slug: this.$route.params.slug
-        })
+            slug: this.$route.params.slug,
+        });
     },
     methods: {
         onSubmit(articleInput) {
-            const slug = this.$route.params.slug
-            this.$store
-                .dispatch(actionTypes.updateArticle, {articleInput, slug})
-                .then(article => {
-                    this.$router.push({name: 'article', params: {slug: article.slug}})
-                })
-        }
-    }
-}
+            const slug = this.$route.params.slug;
+            this.$store.dispatch(actionTypes.updateArticle, { articleInput, slug }).then((article) => {
+                this.$router.push({ name: 'article', params: { slug: article.slug } });
+            });
+        },
+    },
+};
 </script>
